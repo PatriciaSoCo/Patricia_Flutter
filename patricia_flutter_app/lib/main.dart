f65feefd58c57278478a8d5487e77505ba48457e
PatriciaSoCo <79724335+PatriciaSoCo@users.noreply.github.com>
2023-02-03 12:38:44 -0600

    Images

--- patricia_flutter_app/lib/main.dart
+++ patricia_flutter_app/lib/main.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-  home: Scaffold(
+      home: Scaffold(
     appBar: AppBar(
-      title: Center(child:Text("Flutter Project")),
-
+      title: Center(child: Text("Flutter Project")),
+      backgroundColor: Colors.deepPurpleAccent,
     ),
-    body: Center(child:Text ("Learning",
-      style: TextStyle(fontSize: 24),)
-  )
+    body: Center(
+        child: Image(image:NetworkImage ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt0kNoViG6BVHwA8RiNmoK_NdBJc9STDnsrA&usqp=CAU")
+
+    )),
+    bottomNavigationBar: TextButton(
+        child: Text("Need help?"),
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.deepPurpleAccent,
+          foregroundColor: Colors.amber,
+          textStyle: TextStyle(color: Colors.deepOrangeAccent),
+        ),
+        onPressed: () {}),
   )));
-
 }
-
